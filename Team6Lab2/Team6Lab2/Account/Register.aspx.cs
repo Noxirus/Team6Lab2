a831ef31e4688ab81ad7983ebdbe1971e73c76cd
Jared R. Bellamy <820160@ACDM.DS.SAIT.CA>
2020-02-09 19:28:00 -0700

    fixed registry bug
    fixed a the scopping of the database insert so that it would insert before the page changed

--- Team6Lab2/Team6Lab2/Account/Register.aspx.cs
+++ Team6Lab2/Team6Lab2/Account/Register.aspx.cs
@@ -24,9 +24,13 @@ namespace Team6Lab2.Account
                 //string callbackUrl = IdentityHelper.GetUserConfirmationRedirectUrl(code, user.Id, Request);
                 //manager.SendEmail(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>.");
 
+                //Inserting user into Database Marina
+                SqlDataSourceMarina.Insert();
+
                 signInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
-                SqlDataSourceMarina.Insert();
+                
+                
             }
             else
             {
